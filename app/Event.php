7485cbf4bd9a6a3088c5498793481c31e099e0f6
Jose Manuel Cassola <jcassola@uclv.cu>
2020-09-02 21:34:04 -0400

    Metodo para obtener url de la foto agregado al modelo Event

--- app/Event.php
+++ app/Event.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Event extends Model
 {
     public $timestamps = false;
+
+    public function getUrlPicture(){
+        return url($this->picture);
+        //Para cargar la foto en la vista seria asi:
+        //<img src="{{ $event->getUrlPicture }}" alt="">
+    }
 }
