37195c869e0f9c301227ee92283af37c711b0007
Jose Garcia <jogarcia@tuta.io>
2020-09-06 10:33:14 -0400

    Add automatic image copy to the config

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,6 +14,7 @@ const mix = require("laravel-mix");
 
 mix.js("resources/js/app.js", "public/js")
     .sass("resources/sass/app.scss", "public/css")
+    .copy('resources/assets/images', 'public/images')
     .browserSync({
         proxy: process.env.APP_URL,
         open: false
