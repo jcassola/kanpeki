84b6e625c6fdd0e9992e28a197ddc3bbc87efced
Jose Garcia <jogarcia@tuta.io>
2020-09-06 17:22:57 -0400

    Create basic footer

---
+++ resources/js/components/AppFooter.vue
@@ -0,0 +1,29 @@
+<template>
+  <footer>
+      <div class="container">
+          <div class="footer">
+            <p>Copyright by &copyKanpeki-Sa</p>
+          </div>
+      </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "AppFooter"
+};
+</script>
+
+<style scoped>
+.footer {
+   position: fixed;
+   left: 0;
+   bottom: 0;
+   width: 100%;
+   height: 4em;
+   background-color: #343a40;
+   color: rgba(255, 255, 255, 0.75);
+   text-align: center;
+   padding: 0.5rem 1rem;
+}
+</style>
