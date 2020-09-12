1578a4db9f8db9c7455676495f9a88dc3d93366b
Jose Garcia <jogarcia@tuta.io>
2020-09-12 10:35:57 -0400

    Add artist cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -67,8 +67,39 @@
     </section>
     <section>
         <vue-section title="Artistas" class="bg-dark text-light">
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum molestias ullam fuga quas soluta deleniti voluptatum odio, voluptas impedit sit id eveniet veritatis sunt delectus ipsa, facilis nihil, officia esse.</p>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum molestias ullam fuga quas soluta deleniti voluptatum odio, voluptas impedit sit id eveniet veritatis sunt delectus ipsa, facilis nihil, officia esse.</p>
+            <div class="row">
+                <div class="col-md-12">
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum molestias ullam fuga quas soluta deleniti voluptatum odio, voluptas impedit sit id eveniet veritatis sunt delectus ipsa, facilis nihil, officia esse.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum molestias ullam fuga quas soluta deleniti voluptatum odio, voluptas impedit sit id eveniet veritatis sunt delectus ipsa, facilis nihil, officia esse.</p>
+                </div>
+                <div class="col-md-4 text-dark">
+                    <div class="card">
+                        <img class="card-img-top" src="..." alt="Un artista">
+                        <div class="card-body">
+                            <h5 class="card-title">Nick del artista</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 text-dark">
+                    <div class="card">
+                        <img class="card-img-top" src="..." alt="Un artista">
+                        <div class="card-body">
+                            <h5 class="card-title">Nick del artista</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 text-dark">
+                    <div class="card">
+                        <img class="card-img-top" src="..." alt="Un artista">
+                        <div class="card-body">
+                            <h5 class="card-title">Nick del artista</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </vue-section>
     </section>
     <section>
