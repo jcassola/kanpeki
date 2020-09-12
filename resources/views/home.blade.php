59801fe8d5e16788bb9dad6752f6ec3f16b64cb7
Jose Garcia <jogarcia@tuta.io>
2020-09-12 08:57:25 -0400

    Add events section picture

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,8 +14,15 @@
     </section>
     <section>
         <vue-section title="Eventos" class="bg-dark text-light">
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum molestias ullam fuga quas soluta deleniti voluptatum odio, voluptas impedit sit id eveniet veritatis sunt delectus ipsa, facilis nihil, officia esse.</p>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum molestias ullam fuga quas soluta deleniti voluptatum odio, voluptas impedit sit id eveniet veritatis sunt delectus ipsa, facilis nihil, officia esse.</p>
+            <div class="row">
+                <div class="col-md-6">
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum molestias ullam fuga quas soluta deleniti voluptatum odio, voluptas impedit sit id eveniet veritatis sunt delectus ipsa, facilis nihil, officia esse.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum molestias ullam fuga quas soluta deleniti voluptatum odio, voluptas impedit sit id eveniet veritatis sunt delectus ipsa, facilis nihil, officia esse.</p>
+                </div>
+                <div class="col-md-6">
+                    <img src="{{ asset('images/event.jpg') }}" class="img-fluid" alt="Events images" srcset="">
+                </div>
+            </div>
         </vue-section>
     </section>
     <section>
