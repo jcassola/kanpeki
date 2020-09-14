938c127c9e7907e3903bf77c409817ebd9cc211a
Jose Garcia <jogarcia@tuta.io>
2020-09-14 14:49:31 -0400

    Make basic BootstrapProductCard component

---
+++ resources/js/components/BootstrapProductCard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="col-md-3">
+    <div class="card">
+      <img class="card-img-top" src="productPictureUrl" alt="Un producto" />
+      <div class="card-body">
+        <h5 class="card-title">{{ productName }}</h5>
+        <p class="card-text text-secondary">{{ productDescription }}</p>
+        <p class="card-text">{{ productPrice }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [
+    "productPictureUrl",
+    "productName",
+    "productDescription",
+    "productPrice"
+  ],
+  name: "BootstrapProductCard"
+};
+</script>
