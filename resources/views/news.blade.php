ec31ba743a9c6ce481ebfbe8ecc01cd2a54bf2a9
Jose Garcia <jogarcia@tuta.io>
2020-09-14 15:56:53 -0400

    Make basic news view

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -2,19 +2,24 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+    <div class="row">
+        <div class="col-md-6">
             <div class="card">
-                <div class="card-header">Dashboard</div>
-
+                <img class="card-img-top" src="..." alt="Un producto">
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+                    <h5 class="card-title">Nombre del producto</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Ver en la tienda</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="card">
+                <img class="card-img-top" src="..." alt="Un producto">
+                <div class="card-body">
+                    <h5 class="card-title">Nombre del producto</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Ver en la tienda</a>
                 </div>
             </div>
         </div>
