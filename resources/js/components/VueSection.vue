dc3e2732c3adf9bd858c345b06f09f7b9ee80e2e
Jose Garcia <jogarcia@tuta.io>
2020-09-14 16:38:32 -0400

    Add posibility to remove margin to the section

--- resources/js/components/VueSection.vue
+++ resources/js/components/VueSection.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="jumbotron jumbotron-fluid mb-0">
+  <div class="jumbotron jumbotron-fluid mb-0" :class="{ 'no-margin': !hasMargin }">
     <div class="container">
       <h1>{{ title }}</h1>
       <slot></slot>
@@ -9,7 +9,23 @@
 
 <script>
 export default {
-  props: ["title"],
+  props: {
+    title: {
+      type: String,
+      default: ""
+    },
+    hasMargin: {
+      type: Boolean,
+      default: true
+    }
+  },
   name: "VueSection"
 };
 </script>
+
+<style scoped>
+.no-margin {
+    margin-bottom: 0px;
+    margin-top: 0px;
+}
+</style>
