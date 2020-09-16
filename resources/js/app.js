e14d165451fb8bda14f7cc780bdb00e444611ff0
Jose Garcia <jogarcia@tuta.io>
2020-09-16 17:09:51 -0400

    Format app file

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,5 @@
 /**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
+ * This file loads most javascript dependencies
  */
 
 require("./bootstrap");
@@ -10,26 +8,24 @@ const Vue = require("vue");
 window.Vue = Vue;
 
 /**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
+ * The following block of code imports vue components automatically
  *
  * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
  */
 
 const files = require.context("./", true, /\.vue$/i);
-files
-  .keys()
-  .map((key) =>
-    Vue.component(key.split("/").pop().split(".")[0], files(key).default)
-  );
-
-// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+files.keys().map(key =>
+  Vue.component(
+    key
+      .split("/")
+      .pop()
+      .split(".")[0],
+    files(key).default
+  )
+);
 
 /**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
+ * Creates a vue instance
  */
 
 const app = new Vue({
