c9a331c9c15785038e554f9113ce4f10fa3bb22c
Jose Manuel Cassola <jcassola@uclv.cu>
2020-09-19 10:02:02 -0400

    Seeders terminados. Again.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
+        $this->call(AuthorsTableSeeder::class);
+        $this->call(EventsTableSeeder::class);
+        $this->call(ItemsTableSeeder::class);
+        $this->call(NewsTableSeeder::class);
     }
 }
