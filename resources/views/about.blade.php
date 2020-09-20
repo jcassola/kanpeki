9d7df980e914810d15ed3773f3554221306c21a7
Jose Garcia <jogarcia@tuta.io>
2020-09-19 23:45:15 -0400

    Take out repeated title

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,7 +4,6 @@
 <vue-section title="Acerca de nosotros">
     <div class="row justify-content-center">
         <div class="col-md-12">
-            <h1>Acerca de Nosotros</h1>
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus, ea nisi! Deleniti, eaque necessitatibus! Voluptatum aspernatur ad laborum beatae accusamus sed eligendi inventore cupiditate facilis, sequi, odit exercitationem, neque impedit!</p>
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus, ea nisi! Deleniti, eaque necessitatibus! Voluptatum aspernatur ad laborum beatae accusamus sed eligendi inventore cupiditate facilis, sequi, odit exercitationem, neque impedit!</p>
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus, ea nisi! Deleniti, eaque necessitatibus! Voluptatum aspernatur ad laborum beatae accusamus sed eligendi inventore cupiditate facilis, sequi, odit exercitationem, neque impedit!</p>
