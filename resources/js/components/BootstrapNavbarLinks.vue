65e12358f4fe4c612b3350e018e4b6cf88bf3a27
Jose Garcia <jogarcia@tuta.io>
2020-09-22 17:49:16 -0400

    Make navbar a vue component

---
+++ resources/js/components/BootstrapNavbarLinks.vue
@@ -0,0 +1,31 @@
+<template>
+  <ul class="navbar-nav ml-auto">
+    <li class="nav-item">
+      <a class="nav-link active" href="/">Inicio</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="news">Noticias</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="events">Eventos</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="store">Tienda</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="authors">Artistas</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="about">Contactos</a>
+    </li>
+    <!-- Authentication Links -->
+    <slot></slot>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ["userName"],
+  name: "BootstrapNavbarLinks"
+};
+</script>
