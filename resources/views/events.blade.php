2d3f62e9f6c80f20065f9a938ee4eb0e6a3dd5a7
Jose Garcia <jogarcia@tuta.io>
2020-09-23 10:36:58 -0400

    Use the new route section

--- resources/views/events.blade.php
+++ resources/views/events.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.app')
 
+@section('route', Route::currentRouteName())
+
 @section('content')
 <vue-section title="Eventos">
     <div class="row justify-content-center">
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.app')
 
+@section('route', Route::currentRouteName())
+
 @section('content')
 
 <div class="container">
