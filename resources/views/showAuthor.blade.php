ff598fde6777555f9b4e5f738aa62cf8ce08370c
Jose Garcia <jogarcia@tuta.io>
2020-09-25 15:11:00 -0400

    Make basic author details view

---
+++ resources/views/showAuthor.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="row">
+        <div class="col-md-3">
+            <vue-round-image src="{{ $author->image }}"></vue-round-image>
+            <h4>{{ $author->nick }}</h4>
+            <p>{{ $author->category }}</p>
+        </div>
+        <div class="col-md-9">
+        <p>{{ $author->name }}</p>
+        <p>{{ $author->description }}</p>
+        </div>
+    </div>
+@endsection
