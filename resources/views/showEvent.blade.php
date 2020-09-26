5753bbafadd6007910be83801d976b96d47e1330
Jose Garcia <jogarcia@tuta.io>
2020-09-25 21:41:30 -0400

    Change layout

--- resources/views/showEvent.blade.php
+++ resources/views/showEvent.blade.php
@@ -1,17 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="row">
-        <div class="col-md-4">
-            <h4>{{ $event->title }}</h4>
-            <p>{{ $event->when }}</p>
-        </div>
-        <div class="col-md-8"></div>
-            <img src="{{$event->getUrlPicture()}}" alt="">
-        </div>
-{{--        <h5>Descripción</h5>--}}
-        <div class="col-md-12">
-            <p>{{ $event->description }}</p>
+    <vue-spacer space="20"></vue-spacer>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <h1>{{ $event->title }}</h1>
+                <p>{{ $event->when }}</p>
+                <p>{{ $event->description }}</p>
+            </div>
+            <div class="col-md-8">
+                <img class="img-fluid" src="{{$event->getUrlPicture()}}" alt="">
+            </div>
         </div>
     </div>
+    <vue-spacer space="20"></vue-spacer>
 @endsection
