4867c78ceba9f749251dbc188a1a777cf4d0e86d
Jose Garcia <jogarcia@tuta.io>
2020-10-11 21:10:37 -0400

    Rearrange about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,13 +5,8 @@
 @section('content')
 <vue-section title="Acerca de nosotros">
     <div class="row justify-content-center">
-        <div class="col-md-12">
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus, ea nisi! Deleniti, eaque necessitatibus! Voluptatum aspernatur ad laborum beatae accusamus sed eligendi inventore cupiditate facilis, sequi, odit exercitationem, neque impedit!</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus, ea nisi! Deleniti, eaque necessitatibus! Voluptatum aspernatur ad laborum beatae accusamus sed eligendi inventore cupiditate facilis, sequi, odit exercitationem, neque impedit!</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus, ea nisi! Deleniti, eaque necessitatibus! Voluptatum aspernatur ad laborum beatae accusamus sed eligendi inventore cupiditate facilis, sequi, odit exercitationem, neque impedit!</p>
-        </div>
         <div class="col-md-4">
-            <img src="{{ asset('/images/kampeki-kun.jpg') }}" class="img-fluid" alt="Responsive image">
+            <p>Somos un proyecto Santiaguero que abarca la cultura pop asiatica y occidental (se incluyen la cultura pop y tradicional japonesa, coreana, china,etc; además de los videojuegos, comics, literatura en general, la cinefilia y la música de corte mundial)</p>
         </div>
         <div class="col-md-4">
             <img src="{{ asset('/images/kampeki-kun.jpg') }}" class="img-fluid" alt="Responsive image">
